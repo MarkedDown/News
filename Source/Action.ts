fffe6b0f7ea8b3a6fb67bb6d837aada50003a1c7
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2023-01-08 17:45:06 -0500

    Update Action.ts

--- Source/Action.ts
+++ Source/Action.ts
@@ -12,5 +12,5 @@ const config = Deno.env.get('INPUT_CONFIG');
 log('Config',config,Deno.env.toObject());
 
 if(config)
-    log(await Deno.readTextFile(config));
+    log(await Deno.readTextFile(Deno.env.get('GITHUB_ENV') as string));
 
