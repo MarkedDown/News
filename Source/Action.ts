1cfabd6c5092360ed62ac8a6c804ec1f5694cb36
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2023-01-08 16:59:05 -0500

    Config Testing

--- Source/Action.ts
+++ Source/Action.ts
@@ -4,3 +4,13 @@ const { log } = console;
 
 
 log('Testing',import.meta.url);
+
+
+
+const config = Deno.env.get('INPUT_CONFIG');
+
+log('Config',config);
+
+if(config)
+    log(await Deno.readTextFile(config));
+
